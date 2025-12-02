47c379a859c01646408dd7bbd844bab1b8ddcac1
Enmanuel-caraballo <enmanuelcm03@gmail.com>
2025-12-01 23:39:57 -0500

    chip-component + css

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TabComponentComponent } from './components/tab-component/tab-component.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ChipComponent } from './components/chip/chip.component';
 
-const components = [TabComponentComponent, HeaderComponent];
+import {
+  register as registerSwiperElement
+} from 'swiper/element/bundle';
+registerSwiperElement();
+
+const components = [TabComponentComponent, HeaderComponent, ChipComponent];
 const modules = [IonicModule, RouterModule];
 
 @NgModule({
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   declarations: [components],
   imports: [
     CommonModule,
